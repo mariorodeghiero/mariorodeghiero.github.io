1b5f894ebdc9e67a6cbf786ac6568ca1e353c1af
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-09-28 23:49:56 -0300

    update search, footer and @media

---
+++ dist/js/search.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("container-search").style.display = "block";
+    document.getElementById("search").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("container-search").style.display = "none";
+    document.getElementById("search").style.display = "block";
+}
