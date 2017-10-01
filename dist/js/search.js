3c3ffb30ec915f6cace3f05252ed99070d19922c
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-10-01 00:04:58 -0300

    set filter to search

--- dist/js/search.js
+++ dist/js/search.js
@@ -1,9 +1,15 @@
+var openSearch = document.getElementById('search-btn');
+openSearch.addEventListener('click', openNav);
+
+var closeSearch = document.getElementById('close-btn');
+closeSearch.addEventListener('click', closeNav);
+
 function openNav() {
     document.getElementById("container-search").style.display = "block";
-    document.getElementById("search").style.display = "none";
+    document.getElementById("search-btn").style.display = "none";
 }
 
 function closeNav() {
     document.getElementById("container-search").style.display = "none";
-    document.getElementById("search").style.display = "block";
+    document.getElementById("search-btn").style.display = "block";
 }
---
+++ src/js/search-post.js
@@ -0,0 +1,38 @@
+
+const searchInput = document.getElementById('search-btn');
+searchInput.addEventListener('click', getPosts);
+
+const findPost = document.getElementById('search-input');
+findPost.addEventListener('keyup', findMatches);
+
+function getPosts() {
+    fetch('posts.json')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '';
+            data.forEach(function (post) {
+                output +=
+                    `
+                    <li class="list-group-item animated fadeInLeft"><a href="#">${post.title}</a></li><hr class="animated fadeInLeft">
+                    `;
+            });
+            document.getElementById('output').innerHTML = output;
+        });
+}
+
+function findMatches() {
+    let filter = findPost.value.toUpperCase();
+    let ul = document.getElementById('output');
+    let li = ul.getElementsByTagName('li');
+    let hr = document.getElementsByTagName('hr');
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+            hr[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+            hr[i].style.display = "none";
+        }
+    }
+}
