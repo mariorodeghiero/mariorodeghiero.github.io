e0fea0189e64aa63996224d49dcd9739dee5427c
MarioRodeghiero <mario.rodeghiero@gmail.com>
2017-10-02 23:59:16 -0300

    update layout, more search ES6

--- src/js/search-post.js
+++ src/js/search-post.js
@@ -6,7 +6,7 @@ const findPost = document.getElementById('search-input');
 findPost.addEventListener('keyup', findMatches);
 
 function getPosts() {
-    fetch('db/posts.json')
+    fetch('dist/db/posts.json')
         .then((res) => res.json())
         .then((data) => {
             let output = '';
